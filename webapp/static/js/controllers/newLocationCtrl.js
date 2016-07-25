67b6038d14f8a32b9601ab88b929112038167a95
Alexey Porotnikov <alexey.porotnikov@gmail.com>
2016-07-25 22:06:47 +0300

    add typeahead feature to the location search box via WU Autocompletion API

--- webapp/static/js/controllers/newLocationCtrl.js
+++ webapp/static/js/controllers/newLocationCtrl.js
@@ -1,135 +1,154 @@
-
 app.controller('NewLocationCtrl', ['$scope', '$rootScope', '$http', '$log', '$timeout',
-function ($scope, $rootScope, $http, $log, $timeout) {
-    $scope.lookback = 10;
+    function ($scope, $rootScope, $http, $log, $timeout) {
+        $scope.lookback = 10;
 //    $scope.lookforward = 10;
 
-    $scope.gridOptions = {
-        minRowsToShow: 5,
-        enableSorting: false,
-        enableRowSelection: true,
-        multiSelect: false,
-        rowHeight: 40,
-        columnDefs: [
-            {field: 'name'},
-            {field: 'city'},
-            {name: 'Country', field: 'country_name'}
-        ]
-    };
+        $scope.gridOptions = {
+            minRowsToShow: 5,
+            enableSorting: false,
+            enableRowSelection: true,
+            multiSelect: false,
+            rowHeight: 40,
+            columnDefs: [
+                {field: 'name'},
+                {field: 'city'},
+                {name: 'Country', field: 'country_name'}
+            ]
+        };
 
-    $scope.search = function () {
-        $http({
-            url: $SCRIPT_ROOT + '/locations/geolookup',
-            method: "GET",
-            params: {query: $scope.query}
-        })
-            .then(function (data) {
-                    if ('error' in data.data) {
-                        alertify.error(data.data.error);
-                    } else {
-                        console.log(data.data.data);
-                        var result = data.data.data;
-                        $scope.manyLocations = 'results' in result.response;
-                        $scope.oneLocation = 'location' in result;
-                        $scope.notFound = result.response.error;
-                        if ($scope.oneLocation) {
-                            $scope.location = result.location;
-                            $scope.name = result.location.city;
-                            var mapOptions = {
-                                zoom: 7,
-                                center: new google.maps.LatLng($scope.location.lat, $scope.location.lon),
-                                mapTypeId: google.maps.MapTypeId.ROADMAP
-                            };
-                            // ROADMAP, SATELLITE, HYBRID, TERRAIN
-                            $timeout(function () {
-                                map = new google.maps.Map(document.getElementById('gmap_canvas'), mapOptions);
-                                marker = new google.maps.Marker({
-                                    map: map,
-                                    position: new google.maps.LatLng($scope.location.lat, $scope.location.lon)
-                                });
-                            }, 1000);
-
-                        }
-                        if ($scope.manyLocations) {
-                            $scope.gridOptions.data = result.response.results;
-                        }
+        $scope.getSuggestions = function (query) {
+            return $http.jsonp('http://autocomplete.wunderground.com/aq', {
+                    params: {
+                        query: query,
+                        cb: 'JSON_CALLBACK'
                     }
-                },
-                function (error) {
-                    alertify.error(error);
-                });
-    };
-
-    $scope.gridOptions.onRegisterApi = function (gridApi) {
-        //set gridApi on scope
-        $scope.gridApi = gridApi;
-        gridApi.selection.on.rowSelectionChanged($scope, function (row) {
-            if (row.isSelected) {
-                var zmw = row.entity.zmw;
-                $http({
-                    url: $SCRIPT_ROOT + '/locations/geolookup',
-                    method: "GET",
-                    params: {query: 'zmw:' + zmw}
                 })
-                    .then(function (data) {
-                            if ('error' in data.data) {
-                                alertify.error(data.data.error);
-                            } else {
-                                var result = data.data.data;
-                                $scope.oneLocation = 'location' in result;
-                                $scope.notFound = result.response.error;
-                                if ($scope.oneLocation) {
-                                    $scope.location = result.location;
-                                    $scope.name = result.location.city;
-                                    var mapOptions = {
-                                        zoom: 7,
-                                        center: new google.maps.LatLng($scope.location.lat, $scope.location.lon),
-                                        mapTypeId: google.maps.MapTypeId.ROADMAP
-                                    };
-                                    // ROADMAP, SATELLITE, HYBRID, TERRAIN
-                                    $timeout(function () {
-                                        map = new google.maps.Map(document.getElementById('gmap_canvas'), mapOptions);
-                                        marker = new google.maps.Marker({
-                                            map: map,
-                                            position: new google.maps.LatLng($scope.location.lat, $scope.location.lon)
-                                        });
-                                    }, 1000);
-                                }
-                            }
-                        },
-                        function (error) {
-                            alertify.error(error);
-                        });
-            } else {
-                $scope.oneLocation = false;
-            }
-        });
-
-    };
-
-    $scope.addLocation = function () {
-        $('#new-location-dialog').modal('hide');
-        $scope.location.name = $scope.name;
-        $scope.location.lookback = $scope.lookback;
-//        $scope.location.lookforward = $scope.lookforward;
-        alertify.message('Wait until data will be loaded');
-        $http({
-            url: $SCRIPT_ROOT + '/locations',
-            method: "POST",
-            headers: {'Content-Type': 'application/json'},
-            data: JSON.stringify($scope.location)
-        })
-            .then(function (data) {
-                    if ('error' in data.data) {
-                        alertify.error(data.data.error);
-                    } else {
-                        alertify.success('OK');
-                        $rootScope.$broadcast('updateLocations');
-                    }
-                },
-                function (error) {
-                    alertify.error(error);
+                .then(function (response) {
+                    return response.data.RESULTS.map(function (item) {
+                        return item;
+                    });
                 });
-    };
+        };
 
-}]);
+        $scope.typeaheadSelected = function ($item, $model, $label, $event) {
+            $scope.query = $item.name;
+            $scope.search('zmw:' + $item.zmw);
+        }
+
+        $scope.search = function (query) {
+            var query1 = query ? query : $scope.query;
+            $http({
+                url: $SCRIPT_ROOT + '/locations/geolookup',
+                method: "GET",
+                params: {query: query1}
+            })
+                .then(function (data) {
+                        if ('error' in data.data) {
+                            alertify.error(data.data.error);
+                        } else {
+                            console.log(data.data.data);
+                            var result = data.data.data;
+                            $scope.manyLocations = 'results' in result.response;
+                            $scope.oneLocation = 'location' in result;
+                            $scope.notFound = result.response.error;
+                            if ($scope.oneLocation) {
+                                $scope.location = result.location;
+                                $scope.name = result.location.city;
+                                var mapOptions = {
+                                    zoom: 7,
+                                    center: new google.maps.LatLng($scope.location.lat, $scope.location.lon),
+                                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                                };
+                                // ROADMAP, SATELLITE, HYBRID, TERRAIN
+                                $timeout(function () {
+                                    map = new google.maps.Map(document.getElementById('gmap_canvas'), mapOptions);
+                                    marker = new google.maps.Marker({
+                                        map: map,
+                                        position: new google.maps.LatLng($scope.location.lat, $scope.location.lon)
+                                    });
+                                }, 1000);
+
+                            }
+                            if ($scope.manyLocations) {
+                                $scope.gridOptions.data = result.response.results;
+                            }
+                        }
+                    },
+                    function (error) {
+                        alertify.error(error);
+                    });
+        };
+
+        $scope.gridOptions.onRegisterApi = function (gridApi) {
+            //set gridApi on scope
+            $scope.gridApi = gridApi;
+            gridApi.selection.on.rowSelectionChanged($scope, function (row) {
+                if (row.isSelected) {
+                    var zmw = row.entity.zmw;
+                    $http({
+                        url: $SCRIPT_ROOT + '/locations/geolookup',
+                        method: "GET",
+                        params: {query: 'zmw:' + zmw}
+                    })
+                        .then(function (data) {
+                                if ('error' in data.data) {
+                                    alertify.error(data.data.error);
+                                } else {
+                                    var result = data.data.data;
+                                    $scope.oneLocation = 'location' in result;
+                                    $scope.notFound = result.response.error;
+                                    if ($scope.oneLocation) {
+                                        $scope.location = result.location;
+                                        $scope.name = result.location.city;
+                                        var mapOptions = {
+                                            zoom: 7,
+                                            center: new google.maps.LatLng($scope.location.lat, $scope.location.lon),
+                                            mapTypeId: google.maps.MapTypeId.ROADMAP
+                                        };
+                                        // ROADMAP, SATELLITE, HYBRID, TERRAIN
+                                        $timeout(function () {
+                                            map = new google.maps.Map(document.getElementById('gmap_canvas'), mapOptions);
+                                            marker = new google.maps.Marker({
+                                                map: map,
+                                                position: new google.maps.LatLng($scope.location.lat, $scope.location.lon)
+                                            });
+                                        }, 1000);
+                                    }
+                                }
+                            },
+                            function (error) {
+                                alertify.error(error);
+                            });
+                } else {
+                    $scope.oneLocation = false;
+                }
+            });
+
+        };
+
+        $scope.addLocation = function () {
+            $('#new-location-dialog').modal('hide');
+            $scope.location.name = $scope.name;
+            $scope.location.lookback = $scope.lookback;
+//        $scope.location.lookforward = $scope.lookforward;
+            alertify.message('Wait until data will be loaded');
+            $http({
+                url: $SCRIPT_ROOT + '/locations',
+                method: "POST",
+                headers: {'Content-Type': 'application/json'},
+                data: JSON.stringify($scope.location)
+            })
+                .then(function (data) {
+                        if ('error' in data.data) {
+                            alertify.error(data.data.error);
+                        } else {
+                            alertify.success('OK');
+                            $rootScope.$broadcast('updateLocations');
+                        }
+                    },
+                    function (error) {
+                        alertify.error(error);
+                    });
+        };
+
+    }]);
