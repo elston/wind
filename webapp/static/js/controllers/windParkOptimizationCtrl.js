66c59e5dfdca3bc644fc2e6d4e2a7a7d5edb6481
Alexey Porotnikov <alexey.porotnikov@gmail.com>
2016-08-24 21:50:34 +0300

    plot optimization results

--- webapp/static/js/controllers/windParkOptimizationCtrl.js
+++ webapp/static/js/controllers/windParkOptimizationCtrl.js
@@ -1,7 +1,7 @@
 /*global app,$SCRIPT_ROOT,alertify,Highcharts*/
 
-app.controller('WindParkOptimizationCtrl', ['$scope', '$interval', 'windparkService',
-    function ($scope, $interval, windparkService) {
+app.controller('WindParkOptimizationCtrl', ['$scope', '$interval', '$timeout', 'windparkService',
+    function ($scope, $interval, $timeout, windparkService) {
         'use strict';
 
         $scope.startDisabled = false;
@@ -18,11 +18,141 @@ app.controller('WindParkOptimizationCtrl', ['$scope', '$interval', 'windparkServ
             if ( angular.isDefined(stopRefresh) ) return;
 
             stopRefresh = $interval(function() {
-                $scope.refresh();
+                refresh();
             }, 2000);
         };
 
-        $scope.refresh = function() {
+        var refreshCharts = function (data) {
+            var Pd_series = [];
+            for (var i = 0; i < data.Pd.length; i++) {
+                Pd_series.push({
+                    data: data.Pd[i],
+                    name: 'd' + i
+                });
+            }
+
+            var Pa_series = [];
+            for (var i = 0; i < data.Pa.length; i++) {
+                for (var j = 0; j < data.Pa[i].length; j++) {
+                        Pa_series.push({
+                            data: data.Pa[i][j],
+                            name: 'd' + i + ',l' + j
+                    });
+                }
+            }
+
+            var Ps_series = [];
+            for (var i = 0; i < data.Ps.length; i++) {
+                for (var j = 0; j < data.Ps[i].length; j++) {
+                        Ps_series.push({
+                            data: data.Ps[i][j],
+                            name: 'd' + i + ',l' + j
+                    });
+                }
+            }
+
+            $timeout(function () {
+                            $scope.chart1 = new Highcharts.Chart({
+                                chart: {
+                                    renderTo: 'da-volumes',
+                                    animation: false,
+                                    type: 'column'
+                                },
+                                title: {
+                                    text: 'Day ahead volumes'
+                                },
+                                credits: {
+                                    enabled: false
+                                },
+                                plotOptions: {
+                                    column: {
+                                        animation: false,
+                                        marker: {
+                                            enabled: false
+                                        }
+                                    }
+                                },
+                                yAxis: [{
+                                    title: {
+                                        text: 'Volume'
+                                    }
+                                }],
+                                series: Pd_series
+                            });
+
+                            $scope.chart2 = new Highcharts.Chart({
+                                chart: {
+                                    renderTo: 'am-volumes',
+                                    animation: false,
+                                    type: 'column'
+                                },
+                                title: {
+                                    text: 'Adjustment market volumes'
+                                },
+                                credits: {
+                                    enabled: false
+                                },
+                                plotOptions: {
+                                    line: {
+                                        animation: false,
+                                        marker: {
+                                            enabled: false
+                                        }
+                                    }
+                                },
+                                yAxis: [{
+                                    title: {
+                                        text: 'Volume'
+                                    }
+                                }],
+                                series: Pa_series
+                            });
+
+                            $scope.chart3 = new Highcharts.Chart({
+                                chart: {
+                                    renderTo: 'total-volumes',
+                                    animation: false,
+                                    type: 'column'
+                                },
+                                title: {
+                                    text: 'Total volumes'
+                                },
+                                credits: {
+                                    enabled: false
+                                },
+                                plotOptions: {
+                                    line: {
+                                        animation: false,
+                                        marker: {
+                                            enabled: false
+                                        }
+                                    }
+                                },
+                                yAxis: [{
+                                    title: {
+                                        text: 'Volume'
+                                    }
+                                }],
+                                series: Ps_series
+                            });
+
+                        }
+                       );
+
+        };
+
+        var refreshOptimizationResults = function() {
+            windparkService.optimizationResults($scope.windpark.id)
+                .then(function (data) {
+                    $scope.optimizationResults = data;
+                    refreshCharts(data);
+                },
+                function (error) {
+                    alertify.error(error);
+                });
+        };
+
+        var refresh = function() {
             windparkService.optimizationStatus($scope.windpark.id)
                 .then(function (statusData) {
                     $scope.statusData = statusData;
@@ -36,13 +166,7 @@ app.controller('WindParkOptimizationCtrl', ['$scope', '$interval', 'windparkServ
                     if(statusData.isFinished || statusData.isFailed) {
                         $interval.cancel(stopRefresh);
                         stopRefresh = undefined;
-                        windparkService.optimizationResults($scope.windpark.id)
-                            .then(function (data) {
-                                $scope.optimizationResults = data;
-                            },
-                            function (error) {
-                                alertify.error(error);
-                            });
+                        refreshOptimizationResults();
                     }
                 },
                 function (error) {
@@ -52,15 +176,8 @@ app.controller('WindParkOptimizationCtrl', ['$scope', '$interval', 'windparkServ
                 });
         };
 
-        windparkService.optimizationResults($scope.windpark.id)
-            .then(function (data) {
-                $scope.optimizationResults = data;
-            },
-            function (error) {
-                alertify.error(error);
-            });
-
-        $scope.refresh();
+        refreshOptimizationResults();
+        refresh();
 
         $scope.$on('$destroy', function() {
             if (angular.isDefined(stopRefresh)) {
