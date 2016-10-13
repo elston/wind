230a583ebc4b12c0256c04ae9537f4e35f8d710a
Alexey Porotnikov <alexey.porotnikov@gmail.com>
2016-10-13 18:50:51 +0300

    reload static files on change

--- run_server.py
+++ run_server.py
@@ -15,5 +15,16 @@ log.setLevel(logging.INFO if logging_level == logging.DEBUG else logging.WARN)  
 # logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO if logging_level == logging.DEBUG else logging.WARN)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=12080,
-            debug=logging_level == logging.DEBUG, threaded=True)
+    extra_dirs = ['webapp/static/css', 'webapp/static/img', 'webapp/static/js', 'webapp/templates', ]
+    extra_files = extra_dirs[:]
+    for extra_dir in extra_dirs:
+        for dirname, dirs, files in os.walk(extra_dir):
+            for filename in files:
+                filename = os.path.join(dirname, filename)
+                if os.path.isfile(filename):
+                    extra_files.append(filename)
+    app.run(host='0.0.0.0',
+            port=12080,
+            debug=logging_level == logging.DEBUG,
+            threaded=True,
+            extra_files=extra_files)
